0d46ee5f97c791701cad4caf414d49acc3780733
Petar Ivanov <pepincho95@gmail.com>
2018-02-27 15:31:09 +0200

    Add formspree form

---
+++ js/formspree.js
@@ -0,0 +1,26 @@
+$(document).on('ready', function () { 
+  
+  var $contactForm = $('#contact_form');
+  
+  $contactForm.submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+      url: '//formspree.io/pepincho95@gmail.com',
+      method: 'POST',
+      data: $(this).serialize(),
+      dataType: 'json',
+      beforeSend: function() {
+        $contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+      },
+      success: function(data) {
+        $contactForm.find('.alert--loading').hide();
+        $contactForm.append('<div class="alert alert--success">Thank you for your message!</div>');
+        $contactForm[0].reset();
+      },
+      error: function(err) {
+        $contactForm.find('.alert--loading').hide();
+        $contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+      }
+    });
+  });
+});
