936ab0f8cb34a0af53ebda2c24f36c8ca7dc919c
Petar Ivanov <pepincho95@gmail.com>
2018-06-22 12:55:35 +0300

    Add minified js files and cdn links

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-$(document).on('ready', function () { 
-  
+$(document).on('ready', function () {
+
   var $contactForm = $('#contact_form');
-  
+
   $contactForm.submit(function(e) {
     e.preventDefault();
     $.ajax({
@@ -24,3 +24,17 @@ $(document).on('ready', function () {
     });
   });
 });
+
+
+function initMap() {
+  // 43.202348,27.9096177,17z
+  var uluru = {lat: 43.202348, lng: 27.9096177};
+  var map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 17,
+  center: uluru
+  });
+  var marker = new google.maps.Marker({
+  position: uluru,
+  map: map
+  });
+}
